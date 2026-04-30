33747210ade8e71dc39c749bee26fb4e21a3488d
Claower <140240956+Claower3D@users.noreply.github.com>
2026-05-01 02:45:18 +0600

    fix: restrict CORS to production domain

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	r.Static("/uploads", "./uploads")
 
 	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://seti-production.up.railway.app")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
